478b679db18f2f4f2569569de30f3c4f29c7060d
Patrick Almeida <patrick@psa-notebook.local>
2024-03-26 17:12:48 -0300

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,18 @@
+<?php
+
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
